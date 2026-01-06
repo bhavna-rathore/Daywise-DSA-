97bb553b8464ee04e44a529b9794a040ca37b7e2
Bhavna Rathore <bhavnagithub@gmail.com>
2026-01-06 19:47:42 +0530

    Day 72 DSA

---
+++ Day 72/Find the highest/lowest frequency element.js
@@ -0,0 +1,27 @@
+class FrequencyCounter {
+    Frequency(arr) {
+        const freqMap = new Map();
+
+        for (let num of arr) {
+            freqMap.set(num, (freqMap.get(num) || 0) + 1);
+        }
+
+        let maxFreq = 0, minFreq = arr.length;
+        let maxEle = 0, minEle = 0;
+
+         for (let [el, count] of freqMap.entries()) {
+            if (count > maxFreq) {
+                maxFreq = count;
+                maxEle = el;
+            }
+
+            if (count < minFreq) {
+                minFreq = count;
+                minEle = el;
+            }
+        }
+
+        return [minEle, maxEle]
+    }
+}
+
