a3d7f35712ac741d36def5d217af44529151e895
Bhavna Rathore <bhavnagithub@gmail.com>
2026-04-20 20:59:53 +0530

    Day 114

---
+++ Day 114/Sum of Subarray Minimums.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var sumSubarrayMins = function(arr) {
+       let n = arr.length;
+       
+       let mod = 1e9 + 7;
+       let sum = 0;
+       for (let i = 0; i < n; i++) {
+           let mini = arr[i];
+           for (let j = i; j < n; j++) {
+               mini = Math.min(mini, arr[j]);
+
+               sum = (sum + mini) % mod;
+           }
+       }
+
+       return sum;
+};
